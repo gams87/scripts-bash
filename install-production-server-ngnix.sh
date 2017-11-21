2a8e57ebd02fd8d31df218ee35bddfa2793a77d3
Alejandro Morera <galejandromorera@gmail.com>
2017-11-20 20:45:53 -0600

    cambios generales

--- install-production-server-ngnix.sh
+++ install-production-server-ngnix.sh
@@ -65,7 +65,7 @@ VAR_SITE_PORT="80"
 # Base de datos
 VAR_DATABASE_USE=1 # 1 => true 0 => false
 VAR_DATABASE_ENGINE="postgresql"
-VAR_DATABASE_USER="$VAR_SITE_user"
+VAR_DATABASE_USER="$VAR_SITE"
 VAR_DATABASE_PASSWORD="123456"
 VAR_DATABASE_PORT_WEB="8081"
 echo -e "\e[32m[Variables definidas correctamente]\e[39m"
@@ -148,11 +148,10 @@ then
 
 		
 		echo -e ""
+		echo -e "Digite o copie y pegue la siguiente linea y presione enter =>"
 		echo -e "============================================================================"
-		echo -e "Digite o copie y pegue la siguiente linea y presione enter => CREATE USER $VAR_DATABASE_USER WITH PASSWORD '$VAR_DATABASE_PASSWORD';"
-		echo -e "Luego digite o copie y pegue el siguiente comando => \\q"
+		echo -e "CREATE USER $VAR_DATABASE_USER WITH PASSWORD '$VAR_DATABASE_PASSWORD';" \\q
 		echo -e "============================================================================"
-		echo -e ""
 		sudo -u postgres psql
 		#sudo -u postgres psql -c "CREATE USER $VAR_DATABASE_USER WITH PASSWORD '$VAR_DATABASE_PASWORD';"
 		
